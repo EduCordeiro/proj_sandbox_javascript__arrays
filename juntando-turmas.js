7fae4b53dae5e706a656ce42735948da27e44556
Eduardo Cordeiro Maia Monteiro <edufacfito@gmail.com>
2022-07-26 18:21:40 -0300

    Concatenando arrays

---
+++ juntando-turmas.js
@@ -0,0 +1,6 @@
+const salaPython = ["Samanta", "André", "Fernanda"];
+const salaJavaScript = ["Maria", "Carlos", "Eduardo"];
+
+const salaUnificada = salaPython.concat(salaJavaScript);
+
+console.log(salaUnificada);
