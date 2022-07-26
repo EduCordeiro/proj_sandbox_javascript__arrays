7cc1268cbd1deb733b33c1354c2ed3cca06d5134
Eduardo Cordeiro Maia Monteiro <edufacfito@gmail.com>
2022-07-26 18:00:33 -0300

    Dividindo com slice

---
+++ dividindo-arrays.js
@@ -0,0 +1,7 @@
+const nomes = ["Maria", "Carlos", "Eduardo", "Samanta", "André", "Fernanda"];
+
+const sala1 = nomes.slice(0, nomes.length / 2);
+const sala2 = nomes.slice(nomes.length / 2);
+
+console.log('Alunos da sala 1: ' + sala1);
+console.log('Alunos da sala 1: ' + sala2);
