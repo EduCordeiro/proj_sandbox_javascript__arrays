b5e576de2b075548fe7d7ec19304205d8a7240b4
Eduardo Cordeiro Maia Monteiro <edufacfito@gmail.com>
2022-07-26 17:44:17 -0300

    Deletando elementos

---
+++ removendo-nota.js
@@ -0,0 +1,19 @@
+const notas = [10, 6, 8, 7];
+
+console.log(notas);
+
+notas.pop();
+
+console.log(notas);
+
+notas.pop();
+
+console.log(notas);
+
+notas.pop();
+
+console.log(notas);
+
+notas.pop();
+
+console.log(notas);
