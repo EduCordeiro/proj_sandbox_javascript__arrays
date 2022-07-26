2ae5d72088c5135429fe00153df2ea3d692c5c7b
Eduardo Cordeiro Maia Monteiro <edufacfito@gmail.com>
2022-07-26 18:27:25 -0300

    Lista com 2 dimensões

---
+++ lista-bidimensional.js
@@ -0,0 +1,9 @@
+const listaDeAlunos = ["joao", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+const notas = [10, 6.5, 8, 7.5, 5, 2];
+
+let listaBidimensional = [listaDeAlunos, notas];
+
+console.log(listaBidimensional);
+
+console.log(listaBidimensional[0][0] + ' sua nota e: ' + listaBidimensional[1][0]);
