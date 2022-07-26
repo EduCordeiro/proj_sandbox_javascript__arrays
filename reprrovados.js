7102069ecde0b56231e67716ef53586ea5863832
Eduardo Cordeiro Maia Monteiro <edufacfito@gmail.com>
2022-07-26 19:39:47 -0300

    Filtrando elementos

--- reprrovados.js
+++ reprrovados.js
@@ -1,6 +1,7 @@
 const nomes = ["joao", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
 const notas = [10, 6.5, 8, 7.5, 5, 2];
 
+//Quando a função callback retorna verdadeiro, ou true, o elemento é adicionado no array de retorno, e quando ela retorna falso, ou false, o elemento é descartado.
 const reprovados = nomes.filter( (aluno, indice) => notas[indice] < 5)
 
 console.log(reprovados);
